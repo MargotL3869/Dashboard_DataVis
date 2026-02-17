c6a68cdc5a25f9701b959cb36444a0c4a9e5abde
MargotL3869 <margot.laignel8@gmail.com>
2026-02-17 17:58:42 +0100

    Dernieres modification

--- Projet/dash/pages/0_Contexte.py
+++ Projet/dash/pages/0_Contexte.py
@@ -12,7 +12,7 @@ layout = html.Div([
     # BLOC 1 : LA PROBLÉMATIQUE
     dbc.Card(
         dbc.CardBody([
-            html.H4("🎯 La Problématique", className="card-title"),
+            html.H4("La Problématique", className="card-title"),
             html.P(
                 "Comment l'analyse comparative multi-échelles (entre villes et entre pays) "
                 "permet-elle de diagnostiquer la vulnérabilité thermique d'un territoire et de situer "
@@ -41,30 +41,30 @@ layout = html.Div([
     html.Hr(),
 
     # BLOC 3 : GUIDE DES PAGES
-    html.H3("🧭 Parcours de l'analyse :"),
+    html.H3("Parcours de l'analyse :"),
     html.Br(),
 
     dbc.Row([
         # Page 1
         dbc.Col(dbc.Card(dbc.CardBody([
-            html.H5("🌡️ 1. Climat Local", className="text-primary"),
+            html.H5("1. Climat Local", className="text-primary"),
             html.P("Diagnostic territorial : Analyse précise des températures historiques pour confirmer le réchauffement à l'échelle locale.")
         ]), className="h-100"), width=4),
 
         # Page 2
         dbc.Col(dbc.Card(dbc.CardBody([
-            html.H5("🏙️ 2. Comparaison Villes", className="text-primary"),
-            html.P("Benchmarking National : Comment notre ville se situe-t-elle par rapport aux autres territoires français ?")
+            html.H5("2. Comparaison Villes", className="text-primary"),
+            html.P("Comment notre ville se situe-t-elle par rapport aux autres villes française de notre région ?")
         ]), className="h-100"), width=4),
 
         # Page 3
         dbc.Col(dbc.Card(dbc.CardBody([
-            html.H5("🌍 3. International", className="text-primary"),
-            html.P("Perspective Globale : Mise en regard des trajectoires climatiques françaises avec les grandes puissances mondiales.")
+            html.H5("3. International", className="text-primary"),
+            html.P("Globale : Mise en regard des trajectoires climatiques françaises avec les grandes puissances mondiales.")
         ]), className="h-100"), width=4),
     ], className="mb-3"),
 
     html.Div([
-        html.Small("⚠️ Note technique : Les données internationales sont basées sur les moyennes annuelles de température terrestre par pays.")
+        html.Small("Note technique : Les données internationales sont basées sur les moyennes annuelles de température terrestre par pays.")
     ], style={'textAlign': 'center', 'color': 'gray', 'marginTop': '20px'})
 ])
